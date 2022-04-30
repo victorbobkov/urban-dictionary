6168757fda42a32e6a4909cd2d0bd06e0550e697
victor bobkov <victorbobkov@gmail.com>
2022-04-30 19:19:48 +0400

    Add definition function

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+const input = document.getElementById('input')
+const button = document.getElementById('button')
+
 const options = {
    method: 'GET',
    headers: {
       'X-RapidAPI-Host': 'mashape-community-urban-dictionary.p.rapidapi.com',
       'X-RapidAPI-Key': 'c6a141eb82msh91076cc2f8ad375p1b2981jsn0a076c3994fe'
    }
-};
+}
 
-fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=wat`, options)
-   .then(response => response.json())
-   .then(response => console.log(response))
-   .catch(err => console.error(err));
+async function findDefinition() {
+   const apiInput = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${input.value}`
+   let response = await fetch(apiInput, options)
+
+   try {
+      const resultObject = await response.json()
+      console.log(resultObject)
+      console.log(resultObject.list[0].definition)
+   } catch (e) {
+      console.log('Error:', e)
+   }
+}
+
+button.addEventListener('click', findDefinition)
