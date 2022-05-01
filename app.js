b7421fd2a6c70be22bf7c48ad37142f796a529b9
victor bobkov <victorbobkov@gmail.com>
2022-05-02 01:56:50 +0400

    feat: Add elements to definition card

--- app.js
+++ app.js
@@ -10,39 +10,48 @@ const options = {
 }
 
 async function findDefinition() {
-   const apiInput = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${input.value}`
+   const apiInput = `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${ input.value }`
    let response = await fetch(apiInput, options)
 
    try {
       const resultObject = await response.json()
-         console.log(resultObject)
-         generateData(resultObject);
+      console.log(resultObject)
+      generateData(resultObject)
    } catch (e) {
       console.log('Error:', e)
    }
 }
 
 function generateData(resultObject) {
-   let defWord = document.getElementById('def');
+   let defWord = document.getElementById('def')
    if (defWord.childNodes.length === 0) {
-      for (let i=0; i < resultObject.list.length; i++ ) {
-         let div = document.createElement('div')
-         div.className = 'definition-word'
-         defWord.appendChild(div);
+      for (let i = 0; i < resultObject.list.length; i++) {
+         let definitionCard = document.createElement('div')
+         definitionCard.className = 'definition-word'
+         defWord.appendChild(definitionCard)
 
-         let defWordStatement = document.createElement('p');
+         let defWordStatement = document.createElement('p')
          defWordStatement.className = 'definition-statement'
-         defWordStatement.textContent = resultObject.list[i].definition
-         div.appendChild(defWordStatement);
+         defWordStatement.textContent = resultObject.list[i].definition.replace(/\[+|\]+/g, '')
+
+         let defWordExample = document.createElement('p')
+         defWordExample.textContent = resultObject.list[i].example.replace(/\[+|\]+/g, '')
+
+         let defWordLink = document.createElement('a')
+         defWordLink.textContent = resultObject.list[i].permalink
+
+         definitionCard.appendChild(defWordStatement)
+         definitionCard.appendChild(defWordExample)
+         definitionCard.appendChild(defWordLink)
          console.log('defwordstatement', defWordStatement)
       }
    } else {
       let definitionCheck = document.getElementsByClassName('definition-statement')
-      for (let i=0; i < resultObject.list.length; i++) {
+      for (let i = 0; i < resultObject.list.length; i++) {
          definitionCheck[i].textContent = resultObject.list[i].definition
-         console.log('definitionCheck', definitionCheck);
+         console.log('definitionCheck', definitionCheck)
       }
    }
 }
 
-button.addEventListener('click', findDefinition);
+button.addEventListener('click', findDefinition)
